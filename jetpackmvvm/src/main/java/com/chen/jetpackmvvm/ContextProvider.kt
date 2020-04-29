76aa8409967408588a4fb97df54555ba0c8aff0f
chen188669 <188669@163.com>
2020-04-29 23:00:35 +0800

    base

---
+++ jetpackmvvm/src/main/java/com/chen/jetpackmvvm/ContextProvider.kt
@@ -0,0 +1,16 @@
+package com.chen.jetpackmvvm
+
+import android.content.Context
+
+class ContextProvider {
+
+    companion object{
+        val mInstance by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED){
+            ContextProvider()
+        }
+    }
+
+    fun getContext() : Context{
+        return AppContextProvider.mContext
+    }
+}
